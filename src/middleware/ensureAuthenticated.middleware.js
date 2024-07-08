21d3df400ea362ac1460ebd62d32fca5d7a2eab0
TobiLean <Tobiloba.tsl@gmail.com>
2024-07-08 03:35:23 +0100

    updated middleware

--- src/middleware/ensureAuthenticated.middleware.js
+++ src/middleware/ensureAuthenticated.middleware.js
@@ -1,38 +1,3 @@
-// import pkg from 'jsonwebtoken';
-//
-// const {verify} = pkg
-// import {PrismaClient} from '@prisma/client';
-//
-// const prisma = new PrismaClient();
-//
-// export const authenticatedMiddleware = async function (req, res, next) {
-//   const authHeader = req.headers.authorization;
-//   // console.log(authHeader);
-//   const auth = authHeader?.split(' ')[1];
-//   console.log(auth)
-//   if (!auth) {
-//     return res.status(401).json({message: 'Unauthorized access'});
-//   }
-//
-//   try {
-//     const user_id = verify(auth, process.env.JWT_SECRET);
-//     console.log("From middleware: ", user_id);
-//     const isValid = await prisma.user.findUnique({
-//       where: {userId: user_id},
-//     });
-//
-//     if (!isValid) {
-//       return res.status(401).json({message: 'Unauthorized'});
-//     }
-//     else {
-//       next();
-//     }
-//   } catch (err) {
-//     return res.status(403).json({message: 'Invalid Token'});
-//   }
-// }
-
-
 import pkg from 'jsonwebtoken';
 
 const {verify} = pkg;
